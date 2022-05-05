807172d76de63e8e8ec06ec83f9b4d5e51c39770
agnieve70 <82817691+agnieve70@users.noreply.github.com>
2022-05-06 01:54:04 +0800

    first commit

---
+++ admin-sidebar.php
@@ -0,0 +1,12 @@
+<!-- Sidebar-->
+<div class="border-end bg-white" id="sidebar-wrapper">
+    <div class="sidebar-heading border-bottom bg-primary text-white">BKEEPER</div>
+    <div class="list-group list-group-flush">
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="admin-dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="admin-my-bkeepers.php"><i class="fa fa-users"></i> My BKeepers</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="admin-find-bkeepers.php"><i class="fa fa-search"></i> Find BKeepers</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="admin-documents.php"><i class="fa fa-file"></i> Documents</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="admin-applicants.php"><i class="fa fa-user-plus"></i> Applicants</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="admin-post-hiring.php"><i class="fa fa-users"></i> Post Hiring</a>
+    </div>
+</div>
---
+++ bkeeper-sidebar.php
@@ -0,0 +1,11 @@
+<!-- Sidebar-->
+<div class="border-end bg-white" id="sidebar-wrapper">
+    <div class="sidebar-heading border-bottom bg-primary text-white">BKEEPER</div>
+    <div class="list-group list-group-flush">
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="bkeeper-dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="bkeeper-find-client.php"><i class="fa fa-search"></i> Find Client</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="bkeeper-clients.php"><i class="fa fa-users"></i> Clients</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="bkeeper-applied.php"><i class="fa fa-user-plus"></i> Applied Clients</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="bkeeper-details.php"><i class="fa fa-file"></i> My Resume</a>
+    </div>
+</div>
